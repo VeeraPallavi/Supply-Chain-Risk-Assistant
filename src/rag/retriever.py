de261dadf7323b5f0059b0dde0821500fe62e34a
VeeraPallavi <pallavinandimandalam@gmail.com>
2026-06-20 16:12:17 +0530

    Update : [Pallavi] - Updated code to retrive relevant documents

--- src/rag/retriever.py
+++ src/rag/retriever.py
@@ -5,15 +5,17 @@ import numpy as np
 from src.rag.embeddings import EmbeddingGenerator
 
 class Retriever:
-
     def __init__(self):
         self.index = faiss.read_index("vector_db/faiss.index")
         with open("vector_db/documents.pkl","rb") as f:
             self.documents = pickle.load(f)
-        
-        self.embedder = EmbeddingGenerator()
+
+        self.embedder = None
 
     def retrieve(self, query, top_k=5):
+        if self.embedder is None:
+            self.embedder = EmbeddingGenerator()
+
         query_embedding = self.embedder.generate([query])
         query_embedding = np.array(query_embedding).astype("float32")
         distances, indices = self.index.search(query_embedding,top_k)
